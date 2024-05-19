c1cd1fbf3baf00a29c4188836eac30ee25ac808f
Yogendra Pratap Singh <yogendrapratapsingh70@gmail.com>
2024-05-19 23:36:32 +0530

    Day-1 for content print learning

---
+++ day-1/contentPrint.py
@@ -0,0 +1,18 @@
+# Displaying a string  
+print("Hello, Python!")
+
+# Displaying multiple values  
+firstName = "Yogendra Pratap"
+lastName = "Singh"
+age = 27
+print("Name:", firstName + " " + lastName, "Age:", age)
+print("Name:", firstName,lastName, "Age:", age)
+
+# Printing variables and literals  
+num1 = 5
+num2 = 7
+print("num1 =", num1, "num2 =", num2, "and", "Sum =", num1+num2)
+
+# Printing with formatting  
+percentage = 85.75  
+print("Score: {:.3f}%".format(percentage))
